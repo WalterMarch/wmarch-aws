1c5cc4e2323de6d96ace8a62d56963033df96857
Walter March <38635451+WalterMarch@users.noreply.github.com>
2018-12-27 16:35:58 -0500

    Update sshtoEC2.sh
    clarify with comment

--- sshtoEC2.sh
+++ sshtoEC2.sh
@@ -2,6 +2,8 @@
 
 # takes an IP address as input
 # ssh to the EC2 instance that IP represents
+# assumes there is a .myssh directory in the user's home directory and
+# in that directory is a file named after the username with -AWS-1.pem appended
 
 myip=$1
 mycmdstart='ssh ec2-user@'
